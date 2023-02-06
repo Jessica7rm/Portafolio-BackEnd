aac532a4120367f6996a7dd74e235916e28f7168
jessica <jessica7rm@gmail.com>
2023-02-06 20:54:52 -0300

    actualizacion

--- jk_portfolio/src/main/java/com/backend/jk_portfolio/Controller/ExperienciaController.java
+++ jk_portfolio/src/main/java/com/backend/jk_portfolio/Controller/ExperienciaController.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://portafolio-jessica7rm.web.app/#")
 @RequestMapping("experiencia")
 @RestController
 public class ExperienciaController {
